b9ae0928bdef59b14175b8cdc9696b969c562764
Luchi Cheng <luchi.cheng@gmail.com>
2020-03-26 15:50:58 -0400

    enhance baseUrl to handle production

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import store from '@/store/store'
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8081/`,
+    baseURL: process.env.NODE_ENV === 'production' ? 'http://91camp.ca/backapi' : 'http://localhost:8081/backapi',
     headers: {
       Authorization: `Bearer ${store.state.token}`
     }
