a86f42073bdf6e16271c887d151b9ba6218b48cf
Luchi Cheng <luchi.cheng@gmail.com>
2020-12-02 16:13:55 -0500

    add news letter for 2020 virtual Boston run

--- src/components/NewsLetter/Index.vue
+++ src/components/NewsLetter/Index.vue
@@ -47,6 +47,27 @@ export default {
     return {
       articles: [
         {
+          title: '91集训营2020云马集_伦马篇',
+          urlToImage: 'https://deafeducation.org.uk/wp-content/uploads/2020/09/Virtual-London-Marathon.jpg',
+          description: '2020这个特殊的年份，马拉松赛道的画面陈旧，色调灰暗，一个又一个重要比赛相继取消，这一切在年初之时就已定下了基调。但跑者对于比赛的渴求和热忱不会随之消减。相反的，越是没得跑，越是想要跑。这种需求随着休赛期的无限延长而愈发强烈。于是，线上马拉松成为跑者那无处安放的比赛热情的宣泄口。世界各大赛以这种特殊的形式得以继续，弥补了现实马拉松的遗憾。',
+          sourceName: '91编辑部',
+          url: 'https://mp.weixin.qq.com/s/UuK1jwmZWV3ObD6UNizBqQ'
+        },
+        {
+          title: '2020云跑波士顿',
+          urlToImage: 'https://pbs.twimg.com/media/EgiMyMkXYAEiSaf?format=jpg&name=4096x4096',
+          description: '2020是不平凡的一年，COVID19疫情肆虐，从年初开始的各项马拉松比赛相继取消，原本四月份进行的波士顿马拉松也延期到九月份并且改为线上进行。91CAMP的第二届波马团及嘉宾共24名跑者，于2020/09/13，用脚步去丈量这个特殊时期里最特色的42公里；希望以跑者精神，为大众祈福，早日驱散疫情，迎来阳光。',
+          sourceName: '91编辑部',
+          url: 'https://mp.weixin.qq.com/s/LEJVYwRPBieg3Py8soI1hg'
+        },
+        {
+          title: '又一年 新一届 -- 91Camp 庆生，2020 委员会换届',
+          urlToImage: 'https://images.unsplash.com/photo-1604868222953-9dbe5fcf4a41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
+          description: '因同样的热爱而相聚，因分享和参与而一起成长，因严谨和自律而共同进步。91Camp在 2020特殊的一年里幸运的组织了小型庆生会，在欢声笑语，团队趣味竞技中迎来了第三个生日； 而作为一个刚刚走过两年历史的 NPO 组织， 新一届的 91 委员会也诞生了，并投票选举出新任主席！',
+          sourceName: '91编辑部',
+          url: 'https://mp.weixin.qq.com/s/0pgUZU5AXSR_JClERYlkzQ'
+        },
+        {
           title: '风雪中的炽热, 沉淀后的飞跃',
           urlToImage: 'https://images.unsplash.com/photo-1585673191607-41c4c09863a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
           description: '编者按：Bill Huang ，多伦多91集训营的一个“草根”跑者 ，2020 年3月1日在美国佛吉尼亚州Newport News马拉松实现Sub250的突破 ！纵观他四年的跑马史，2016 年湖滨马拉松(首马)的406，2017 年湖滨马拉松的336，2018 年巴黎马拉松的320（同年加入91集训营），2019 年伦敦马拉松的258，实现“破3”的里程碑，到今年因东京马取消而在Newport News One City Marathon 的246，通过后天刻苦努力，科学系统训练和时间跑量堆积，逐步成长，最终飞跃；更可贵的是其难得的赤诚之心，严谨的日常总结，勇敢的自我尝试和坚定的跑步初心。对跑步始于兴趣，陷于热爱，忠于勤奋的他，赛记中的感恩，思考和坚持想必才是令这个“地才”不断刷新他自己的跑步极限的秘密。',
